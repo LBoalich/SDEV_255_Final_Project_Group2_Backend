3d8145967fce03ffb974fd5b76c0a57d65f0ba69
LBoalich <lkboalich@gmail.com>
2025-07-12 13:04:40 -0400

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // setup
 const express = require("express")
+// we have to use cors in order to host a front end and backend on the same device
+var cors = require("cors")
 // activate the app variable to be an express server
 const app = express()
-// start the web server
-app.listen(3000, function() {
-    console.log("Listening on port 3000")
-})
+app.use(cors())
+const router = express.Router()
+
 // making an api using routes
+
+// all requests that usually use an api start with /api
+app.use("/api", router)
+app.listen(3000)
