9931c0ae54c3c8cdc7aff3d67ea1ec7b09da1d08
KaleeBurchett <kburchett11@ivytech.edu>
2025-07-16 14:31:11 -0400

    Updated the Login system - need to connect it all through frontend and backend

--- app.js
+++ app.js
@@ -1,14 +1,114 @@
-// setup
-const express = require("express")
-// we have to use cors in order to host a front end and backend on the same device
-var cors = require("cors")
-// activate the app variable to be an express server
-const app = express()
-app.use(cors())
-const router = express.Router()
+const express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose =
+        require("passport-local-mongoose");
+        const cors = require('cors');
+const User = require("./model/User");
+var path = require('path');
+let app = express();
+app.use(cors());
+app.use(express.json());
 
-// making an api using routes
+mongoose.connect("mongodb+srv://kburchett11:Final246@loginsystem.9kr2lp0.mongodb.net/?retryWrites=true&w=majority&appName=LoginSystem");
 
-// all requests that usually use an api start with /api
-app.use("/api", router)
-app.listen(3000)
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(require("express-session")({
+    secret: "Group 2 Final",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.engine('html', require('ejs').renderFile);
+const publicPath = path.join(__dirname, 'public'); 
+app.use(express.static(publicPath));
+
+// Showing home page
+app.get("/", function (req, res) {
+    res.render("index.html");
+});
+
+// Showing register form
+app.get("/register", function (req, res) {
+    res.render("register.html");
+});
+
+// Handling user signup
+app.post("/register", async (req, res) => {
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    return res.status(200).json(user);
+});
+
+// Showing login form
+app.get("/login", function (req, res) {
+    res.render("login.html");
+});
+
+// Handling user login
+app.post("/login", async function (req, res) {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (user) {
+            const result = req.body.password === user.password;
+            if (result) {
+                res.render("student_dashboard.html");
+            } else {
+                res.status(400).json({ error: "password doesn't match" });
+            }
+        } else {
+            res.status(400).json({ error: "User doesn't exist" });
+        }
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/view/student_dashboard.html");
+}
+
+app.get("/student_view_schedule", function (req, res) {
+    res.render("student_view_schedule.html");
+});
+
+app.get("/student_dashboard", function (req, res) {
+    res.render("student_dashboard.html");
+});
+
+app.get("/student_manage_courses", function (req, res) {
+    res.render("student_manage_courses.html");
+});
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Data from backend!' });
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
+
