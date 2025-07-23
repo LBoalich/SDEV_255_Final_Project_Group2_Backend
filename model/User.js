c9ff905d50c7fbd0200e5602ff15be251c0631f0
KaleeBurchett <kburchett11@ivytech.edu>
2025-07-23 16:33:03 -0400

    Must have unique username

--- model/User.js
+++ model/User.js
@@ -4,6 +4,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     username: {
         type: String,
+        unique: true,
         required : true
     },
     password: {
